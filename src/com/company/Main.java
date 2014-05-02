cc50d7f8c478f8248d29661aab28a9ca5ad53cc7
sean74 <scampbell5@msn.com>
2014-05-01 20:46:22 -0400

    Testing

---
+++ src/com/company/Main.java
@@ -0,0 +1,98 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code her
+	/*    System.out.println("Please enter some text.");
+      Scanner bucky = new Scanner(System.in);
+        System.out.println(bucky.nextLine());
+ */
+/*
+        Scanner newScanner = new Scanner(System.in);
+
+        double fnum, snum, answer;
+
+        System.out.println("Please enter first number.");
+
+        fnum = newScanner.nextDouble();
+
+        System.out.println("Please enter second number.");
+
+        snum = newScanner.nextDouble();
+
+        answer = snum + fnum;
+
+        System.out.println(answer);
+
+
+        Scanner bucky = new Scanner(System.in);
+
+        double girls, boys, people, peopleremainder;
+
+        girls = 12;
+        boys = 3;
+        peopleremainder = girls % boys;
+        people = (girls / boys);
+
+
+
+
+        System.out.println(people + " remainder " + peopleremainder);
+
+
+
+        int tuna = 5;
+        int bass = 18;
+
+        ++tuna;
+        System.out.println(++tuna);
+
+
+
+
+
+        int test = 9;
+
+        if (test == 9) {
+
+            System.out.println("Test is equal to 9.");
+
+        }else {
+
+                System.out.println("Test is not equal to 9.");
+
+            }
+        }
+
+
+
+
+
+        int boy, girl;
+
+        boy = 9;
+        girl = 99;
+
+       if (boy > 10 || girl < 60) {
+           System.out.println("You can enter.");
+
+       }else{
+
+           System.out.println("You can not enter.");
+       }
+
+     */
+
+
+
+        int test = 2;
+
+        test++;
+
+        System.out.println(test);
+
+    }
+}
